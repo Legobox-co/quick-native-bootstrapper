5074b73d4dc69d8a736c5ecc6573cba3a70ff766
Chibuike Emmauel Osita <mozart.osita@gmail.com>
2018-06-06 14:27:40 +0100

    multiple updates

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,13 @@ import { Icon } from "native-base";
 import DefaultProps from "../constants/navigation";
 import AppConfig from "../../constants/config";
 
-import SignUpContainer from "../../containers/SignUp";
+import SignUpContainer from "../containers/SignUp";
 import SignUpComponent from "../components/SignUp";
 
-import LoginContainer from "../../containers/Login";
+import LoginContainer from "../containers/Login";
 import LoginComponent from "../components/Login";
 
-import ForgotPasswordContainer from "../../containers/ForgotPassword";
+import ForgotPasswordContainer from "../containers/ForgotPassword";
 import ForgotPasswordComponent from "../components/ForgotPassword";
 
 import AboutComponent from "../components/About";
@@ -19,8 +19,9 @@ import AboutComponent from "../components/About";
 const Index = (
   <Stack>
     <Scene key="login" component={LoginContainer} Layout={LoginComponent}/> 
-    <Scene key="signup" component={SignUpContainer} Layout={SignupComponent}/> 
-    <Scene key="signup" component={ForgotPasswordContainer} Layout={ForgotPasswordComponent}/> 
+    <Scene key="signup" component={SignUpContainer} Layout={SignUpComponent}/> 
+    <Scene key="signup" component={ForgotPasswordContainer} Layout={ForgotPasswordComponent}/>
+    <Scene key="about" component={ForgotPasswordContainer} Layout={ForgotPasswordComponent}/> 
   </Stack>
 );
 
